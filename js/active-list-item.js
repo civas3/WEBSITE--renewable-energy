7a916b318291dd6498fa435b07a1c657eda2a302
Paul C <57236805+civas3@users.noreply.github.com>
2021-02-09 13:58:11 +0000

    UPDATED VERSION

--- js/active-list-item.js
+++ js/active-list-item.js
@@ -1,39 +1,35 @@
-$(document).ready(function () {
-    $(document).on("scroll", onScroll);
-    
-    //smoothscroll
-    $('a[href^="#"]').on('click', function (e) {
-        e.preventDefault();
-        $(document).off("scroll");
-        
-        $('a').each(function () {
-            $(this).removeClass('active');
-        })
-        $(this).addClass('active');
-      
-        var target = this.hash,
-            menu = target;
-        $target = $(target);
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top+2
-        }, 500, 'swing', function () {
-            window.location.hash = target;
-            $(document).on("scroll", onScroll);
-        });
+$(document).ready(function() {
+    $('a[href*=#]').bind('click', function(e) {
+            e.preventDefault(); // prevent hard jump, the default behavior
+
+            var target = $(this).attr("href"); // Set the target as variable
+
+            // perform animated scrolling by getting top-position of target-element and set it as scroll target
+            $('html, body').stop().animate({
+                    scrollTop: $(target).offset().top
+            }, 600, function() {
+                    location.hash = target; //attach the hash (#jumptarget) to the pageurl
+            });
+
+            return false;
     });
 });
 
-function onScroll(event){
-    var scrollPos = $(document).scrollTop();
-    $('#navigation a').each(function () {
-        var currLink = $(this);
-        var refElement = $(currLink.attr("href"));
-        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-            $('#navigation ul li a').removeClass("active");
-            currLink.addClass("active");
-        }
-        else{
-            currLink.removeClass("active");
-        }
+$(window).scroll(function() {
+    var scrollDistance = $(window).scrollTop();
+
+    // Show/hide menu on scroll
+    //if (scrollDistance >= 850) {
+    //		$('nav').fadeIn("fast");
+    //} else {
+    //		$('nav').fadeOut("fast");
+    //}
+
+    // Assign active class to nav links while scolling
+    $('.nav-menu-item').each(function(i) {
+            if ($(this).position().top <= scrollDistance) {
+                    $('.navbar-nav a.active').removeClass('active');
+                    $('.navbar-nav a').eq(i).addClass('active');
+            }
     });
-}
+}).scroll();
--- js/onscroll-fixed.js
+++ js/onscroll-fixed.js
@@ -1,13 +1,20 @@
 
-window.onscroll = function() {myFunction()};
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+  myFunction();
+};
 
-var header = document.getElementById("navigation");
+// Get the header
+var header = document.getElementById("nav");
+
+// Get the offset position of the navbar
 var sticky = header.offsetTop;
 
-function myFunction() {
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction(x) {
   if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
+    header.classList.add("fixed-top");
   } else {
-    header.classList.remove("sticky");
+    header.classList.remove("fixed-top");
   }
 }
